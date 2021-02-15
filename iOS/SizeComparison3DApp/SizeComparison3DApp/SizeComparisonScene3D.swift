2d223238f966cb2cffadd9405265075c91b60210
Marko Ranković <marko@markorankovic.com>
2021-02-15 16:19:59 +0000

    3D size comparison done, camera sequence works as planned

--- iOS/SizeComparison3DApp/SizeComparison3DApp/SizeComparisonScene3D.swift
+++ iOS/SizeComparison3DApp/SizeComparison3DApp/SizeComparisonScene3D.swift
@@ -5,12 +5,13 @@ import ARKit
 
 class SizeComparisonScene3D: UIViewController {
     
-    let scene = SCNScene(named: "art.scnassets/asteroid3.scn")!
+    let scene = SCNScene()
+    
+    let cameraNode = SCNNode()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let cameraNode = SCNNode()
         cameraNode.camera = SCNCamera()
         scene.rootNode.addChildNode(cameraNode)
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
@@ -35,15 +36,48 @@ class SizeComparisonScene3D: UIViewController {
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         scnView.addGestureRecognizer(tapGesture)
-        
-        //initAsteroids()
+                
+        initAsteroids()
+        initCameraSequence()
+    }
+    
+    func initCameraSequence() {
+        let asteroids = scene.rootNode.childNodes.filter{ $0.name == "asteroid" }
+        let sequence = SCNAction.sequence(asteroids.map {
+            var pos = $0.position
+            pos.z = ($0.geometry!.boundingSphere.radius) * 10
+            return SCNAction.sequence([
+                SCNAction.move(
+                    to: pos,
+                    duration: 3
+                ),
+                SCNAction.wait(duration: 3)
+            ])
+        })
+        cameraNode.runAction(sequence)
     }
     
     func initAsteroids() {
-        let box = SCNBox()
-        box.subdivisionLevel = 3
-        let asteroid = SCNNode(geometry: box)
-        scene.rootNode.addChildNode(asteroid)
+        var prevX: CGFloat = 0
+        var prevDiameter: CGFloat = 0
+        for i in 1...10 {
+            let diameter = i
+            
+            let sphere = SCNSphere(radius: CGFloat(diameter) / 2)
+            sphere.isGeodesic = true
+            
+            print(Bundle.main.bundleURL)
+            sphere.firstMaterial!.displacement.contents = UIImage(named: "noise")
+            sphere.firstMaterial!.diffuse.contents = UIImage(named: "asteroid")
+            
+            let asteroid = SCNNode(geometry: sphere)
+            asteroid.name = "asteroid"
+            asteroid.position.x += Float(3 * diameter) / 2 + Float(prevDiameter) + Float(prevX)
+            asteroid.position.y += Float(diameter) / 2
+            scene.rootNode.addChildNode(asteroid)
+            prevDiameter = CGFloat(diameter)
+            prevX = CGFloat(asteroid.position.x)
+        }
     }
     
     @objc
