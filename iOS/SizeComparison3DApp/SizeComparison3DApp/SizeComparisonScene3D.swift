c8ac133b5f0b03b094f3122b6c35264e768462ae
Marko Ranković <marko@markorankovic.com>
2021-02-18 20:34:14 +0000

    Scroll feature now works for size comparison 3D

--- iOS/SizeComparison3DApp/SizeComparison3DApp/SizeComparisonScene3D.swift
+++ iOS/SizeComparison3DApp/SizeComparison3DApp/SizeComparisonScene3D.swift
@@ -7,12 +7,14 @@ class SizeComparisonScene3D: UIViewController {
     
     let scene = SCNScene()
     
-    let cameraNode = SCNNode()
-
+    var cameraNode = SCNNode()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        cameraNode.camera = SCNCamera()
+        let camera = SCNCamera()
+        camera.zFar = 1000
+        cameraNode.camera = camera
         scene.rootNode.addChildNode(cameraNode)
         cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
         
@@ -21,59 +23,50 @@ class SizeComparisonScene3D: UIViewController {
         lightNode.light!.type = .omni
         lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
         scene.rootNode.addChildNode(lightNode)
-        
+                
         let ambientLightNode = SCNNode()
         ambientLightNode.light = SCNLight()
         ambientLightNode.light!.type = .ambient
         ambientLightNode.light!.color = UIColor.darkGray
         scene.rootNode.addChildNode(ambientLightNode)
-                
+        
         let scnView = self.view as! SCNView
         scnView.scene = scene
         scnView.showsStatistics = true
         scnView.backgroundColor = UIColor.black
-        scnView.allowsCameraControl = true
+                        
+        initAsteroids()
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
         scnView.addGestureRecognizer(tapGesture)
-                
-        initAsteroids()
-        initCameraSequence()
+        scnView.addGestureRecognizer(panGesture)
     }
     
-    func initCameraSequence() {
-        let asteroids = scene.rootNode.childNodes.filter{ $0.name == "asteroid" }
-        let sequence = SCNAction.sequence(asteroids.map {
-            var pos = $0.position
-            pos.z = ($0.geometry!.boundingSphere.radius) * 10
-            return SCNAction.sequence([
-                SCNAction.move(
-                    to: pos,
-                    duration: 3
-                ),
-                SCNAction.wait(duration: 3)
-            ])
-        })
-        cameraNode.runAction(sequence)
+    var pointsToScrollThrough: [SCNVector3] {
+        let asteroids = scene.rootNode.childNodes.filter({ $0.name == "asteroid" })
+        return asteroids.map {
+            .init($0.position.x, $0.position.y, $0.position.z + $0.boundingSphere.radius * 10)
+        }
     }
     
     func initAsteroids() {
         var prevX: CGFloat = 0
         var prevDiameter: CGFloat = 0
-        for i in 1...10 {
-            let diameter = i
+        let sizes = (1...10).map({ _ in Int.random(in: 1...30) / 2 }).sorted()
+        for r in sizes {
+            let diameter: CGFloat = CGFloat(r * 2)
             
-            let sphere = SCNSphere(radius: CGFloat(diameter) / 2)
+            let sphere = SCNSphere(radius: CGFloat(r))
             sphere.isGeodesic = true
             
-            print(Bundle.main.bundleURL)
             sphere.firstMaterial!.displacement.contents = UIImage(named: "noise")
             sphere.firstMaterial!.diffuse.contents = UIImage(named: "asteroid")
             
             let asteroid = SCNNode(geometry: sphere)
             asteroid.name = "asteroid"
-            asteroid.position.x += Float(3 * diameter) / 2 + Float(prevDiameter) + Float(prevX)
-            asteroid.position.y += Float(diameter) / 2
+            asteroid.position.x += Float(3 * r) / 2 + Float(prevDiameter) + Float(prevX)
+            asteroid.position.y += Float(r)
             scene.rootNode.addChildNode(asteroid)
             prevDiameter = CGFloat(diameter)
             prevX = CGFloat(asteroid.position.x)
@@ -81,9 +74,55 @@ class SizeComparisonScene3D: UIViewController {
     }
     
     @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
+    func handleTap(_ gestureRecognize: UITapGestureRecognizer) {
     }
     
+    @objc
+    func handlePan(_ gestureRecognize: UIPanGestureRecognizer) {
+        let asteroids = scene.rootNode.childNodes.filter({ $0.name == "asteroid" })
+        let velocity = gestureRecognize.velocity(in: view)
+        let dx = velocity.x
+        print("dx: \(dx)")
+        let asteroidsBetween: (SCNNode?, SCNNode?) = {
+            let lAsteroid = asteroids.filter{ $0.position.x < cameraNode.position.x }.first
+            let rAsteroid = asteroids.filter{ $0.position.x > cameraNode.position.x }.first
+            return (lAsteroid, rAsteroid)
+        }()
+        
+        let baseDx: CGFloat = -5000
+        
+        var v: Float = 0
+        
+        var dz: Float = 0
+        
+        switch asteroidsBetween {
+        case (nil, .some(let r)):
+            if dx >= 0 {
+                v = 0.1
+                cameraNode.position.z = r.position.z + r.boundingSphere.radius * 10
+            } else { v = -0.1 }
+        case (.some(let l), nil):
+            if dx <= 0 {
+                v = 1
+                cameraNode.position.z = l.position.z + l.boundingSphere.radius * 10
+            } else { v = -1 }
+        case (.some(let l), .some(let r)):
+            let speed = Float((CGFloat(r.position.x - l.position.x) / baseDx) * dx)
+            v = dx > 0 ? abs(speed) : -abs(speed)
+            
+            let lPosZ = l.position.z + l.boundingSphere.radius * 10
+            let rPosZ = r.position.z + r.boundingSphere.radius * 10
+
+            let speedZ = Float((CGFloat(rPosZ - lPosZ) / baseDx) * dx)
+            dz = speedZ > 0 ? abs(speedZ) : -abs(speedZ)
+        case (.none, .none): v = 0
+        }
+        
+        print("v: \(v)")
+        cameraNode.position.x += v
+        cameraNode.position.z -= dz
+    }
+
     override var shouldAutorotate: Bool {
         return true
     }
---
+++ iOS/UIPanErrorReproduction/UIPanErrorReproduction/GameViewController.swift
@@ -0,0 +1,90 @@
+//
+//  GameViewController.swift
+//  UIPanErrorReproduction
+//
+//  Created by Marko Rankovic on 17/02/2021.
+//
+
+import UIKit
+import QuartzCore
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    let cameraNode = SCNNode()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // create and add a camera to the scene
+        cameraNode.camera = SCNCamera()
+        scene.rootNode.addChildNode(cameraNode)
+        
+        // place the camera
+        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        
+        // create and add a light to the scene
+        let lightNode = SCNNode()
+        lightNode.light = SCNLight()
+        lightNode.light!.type = .omni
+        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
+        scene.rootNode.addChildNode(lightNode)
+        
+        // create and add an ambient light to the scene
+        let ambientLightNode = SCNNode()
+        ambientLightNode.light = SCNLight()
+        ambientLightNode.light!.type = .ambient
+        ambientLightNode.light!.color = UIColor.darkGray
+        scene.rootNode.addChildNode(ambientLightNode)
+        
+        // retrieve the ship node
+        //let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+        
+        // animate the 3d object
+        //ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
+        
+        // retrieve the SCNView
+        let scnView = self.view as! SCNView
+        
+        // set the scene to the view
+        scnView.scene = scene
+        
+        // allows the user to manipulate the camera
+        //scnView.allowsCameraControl = true
+        
+        // show statistics such as fps and timing information
+        scnView.showsStatistics = true
+        
+        // configure the view
+        scnView.backgroundColor = UIColor.black
+        
+        // add a tap gesture recognizer
+        let tapGesture = UIPanGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        scnView.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    func handleTap(_ gestureRecognize: UIPanGestureRecognizer) {
+        cameraNode.position.x += Float(gestureRecognize.velocity(in: view).x) / 1000
+    }
+    
+    override var shouldAutorotate: Bool {
+        return true
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+}
